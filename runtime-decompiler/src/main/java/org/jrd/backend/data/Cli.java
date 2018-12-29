e4ffbbcd6e163fa88b6138faab2fb5903f11ea32
Radek Maňák <Radek.Manak@protonmail.com>
2018-12-29 18:41:29 +0100

    refactor cli parsing to use commons-cli library

--- runtime-decompiler/src/main/java/org/jrd/backend/data/Cli.java
+++ runtime-decompiler/src/main/java/org/jrd/backend/data/Cli.java
@@ -10,101 +10,22 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
 public class Cli {
 
-
-    public static final String VERBOSE = "-verbose";
-    private static final String LISTJVMS = "-listjvms";
-    private static final String LISTPLUGINS = "-listplugins";
-    private static final String LISTCLASSES = "-listclasses";
-    private static final String BASE64 = "-base64bytes";
-    private static final String BYTES = "-bytes";
-    private static final String DECOMPILE = "-decompile";
-    private static final String HELP = "-help";
-    private static final String H = "-h";
-
-    private final String[] allargs;
     private final VmManager vmManager;
     private final PluginManager pluginManager;
 
-    public Cli(String[] orig, Model model) {
-        this.allargs = orig;
+    public Cli(Model model) {
         this.vmManager = model.getVmManager();
         this.pluginManager = model.getPluginManager();
-        for (String arg : allargs) {
-            String aarg = cleanParameter(arg);
-            if (aarg.equals(HELP) || aarg.equals(H)) {
-                printHelp();
-                System.exit(0);
-            }
-        }
     }
 
-    private static String cleanParameter(String param) {
-        return param.replaceAll("^--*", "-").toLowerCase();
-    }
-
-    public boolean shouldBeVerbose() {
-        for (String arg : allargs) {
-            String aarg = cleanParameter(arg);
-            if (aarg.equals(VERBOSE)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isGui() {
-        return prefilterArgs().isEmpty();
-    }
-
-    private List<String> prefilterArgs() {
-        List<String> args = new ArrayList(allargs.length);
-        for (String arg : allargs) {
-            String aarg = cleanParameter(arg);
-            if (!aarg.equals(VERBOSE)) {
-                args.add(arg);
-            }
-        }
-        return args;
-    }
-
-    public void consumeCli() throws Exception {
-        List<String> args = prefilterArgs();
-        for (int i = 0; i < args.size(); i++) {
-            String arg = args.get(i);
-            arg = cleanParameter(arg);
-            if (arg.equals(LISTJVMS)) {
-                listJvms(args);
-                break;
-            }
-            if (arg.equals(LISTPLUGINS)) {
-                listPlugins(args);
-                break;
-            } else if (arg.equals(LISTCLASSES)) {
-                listClasses(args, i);
-                break;
-            } else if (arg.equals(BYTES) || arg.equals(BASE64)) {
-                boolean bytes = arg.equals(BYTES);
-                printBytes(args, i, bytes);
-                break;
-            } else if (arg.equals(DECOMPILE)) {
-                decompile(args, i);
-                break;
-            } else {
-                printHelp();
-                throw new RuntimeException("Unknown commandline switch " + arg);
-            }
-        }
-    }
-
-    private void decompile(List<String> args, int i) throws Exception {
+    public  void decompile(List<String> args, int i) throws Exception {
         if (args.size() != 4) {
-            throw new RuntimeException(DECOMPILE + " expects exactly three arguments - pid or url of JVM, fully classified class name and decompiler name (as set-up) or decompiler json file, or javap(see help)");
+//            throw new RuntimeException(DECOMPILE + " expects exactly three arguments - pid or url of JVM, fully classified class name and decompiler name (as set-up) or decompiler json file, or javap(see help)");
         }
         String jvmStr = args.get(i + 1);
         String classStr = args.get(i + 2);
@@ -162,7 +83,7 @@ public class Cli {
 
     private void printBytes(List<String> args, int i, boolean bytes) throws IOException {
         if (args.size() != 3) {
-            throw new RuntimeException(BYTES + " and " + BASE64 + " expect exactly two arguments - pid or url of JVM and fully classified class name");
+//            throw new RuntimeException(BYTES + " and " + BASE64 + " expect exactly two arguments - pid or url of JVM and fully classified class name");
         }
         String jvmStr = args.get(i + 1);
         String classStr = args.get(i + 2);
@@ -185,70 +106,45 @@ public class Cli {
         }
     }
 
-    private void listClasses(List<String> args, int i) {
-        if (args.size() != 2) {
-            throw new RuntimeException(LISTCLASSES + " expect exactly one argument - pid or url");
-        }
-        String param = args.get(i + 1);
-        try {
-            VmInfo vmInfo = vmManager.findVmFromPID(param);
-            AgentRequestAction request = VmDecompilerInformationController.createRequest(vmInfo, null, AgentRequestAction.RequestAction.CLASSES);
-            String response = VmDecompilerInformationController.submitRequest(vmManager, request);
-            if (response.equals("ok")) {
-                String[] classes = vmInfo.getVmDecompilerStatus().getLoadedClassNames();
-                for (String clazz : classes) {
-                    System.out.println(clazz);
-                }
-            }
-            if (response.equals("error")) {
-                throw new RuntimeException(VmDecompilerInformationController.CLASSES_NOPE);
+//    public void listClasses(String VMPIDorURL) {
+////        String param = args.get(i + 1);
+//        try {
+//            VmInfo vmInfo = vmManager.findVmFromPID(param);
+//            AgentRequestAction request = VmDecompilerInformationController.createRequest(vmInfo, null, AgentRequestAction.RequestAction.CLASSES);
+//            String response = VmDecompilerInformationController.submitRequest(vmManager, request);
+//            if (response.equals("ok")) {
+//                String[] classes = vmInfo.getVmDecompilerStatus().getLoadedClassNames();
+//                for (String clazz : classes) {
+//                    System.out.println(clazz);
+//                }
+//            }
+//            if (response.equals("error")) {
+//                throw new RuntimeException(VmDecompilerInformationController.CLASSES_NOPE);
+//
+//            }
+//        } catch (NumberFormatException e) {
+//            try {
+//                URL u = new URL(param);
+//                throw new RuntimeException("Remote VM not yet implemented");
+//            } catch (MalformedURLException ee) {
+//                throw new RuntimeException("Second param was supposed to be URL or PID", ee);
+//            }
+//        }
+//    }
 
-            }
-        } catch (NumberFormatException e) {
-            try {
-                URL u = new URL(param);
-                throw new RuntimeException("Remote VM not yet implemented");
-            } catch (MalformedURLException ee) {
-                throw new RuntimeException("Second param was supposed to be URL or PID", ee);
-            }
+    public void listPlugins() {
+        List<DecompilerWrapperInformation> wrappers = pluginManager.getWrappers();
+        for (DecompilerWrapperInformation wrapper : wrappers) {
+            System.out.println(wrapper.getName() + " " + wrapper.getScope() +
+                    "/" + invalidityToString(wrapper.isInvalidWrapper()) + " - " + wrapper.getFileLocation());
         }
     }
 
-    private void listPlugins(List<String> args) {
-        if (args.size() != 1) {
-            throw new RuntimeException(LISTPLUGINS + " does not expect argument");
-        }
-        PluginManager pm = new PluginManager();
-        List<DecompilerWrapperInformation> wrappers = pm.getWrappers();
-        for (DecompilerWrapperInformation dw : wrappers) {
-            System.out.println(dw.getName() + " " + dw.getScope() + "/" + invalidityToString(dw.isInvalidWrapper()) + " - " + dw.getFileLocation());
-        }
-    }
-
-    private void listJvms(List<String> args) {
-        if (args.size() != 1) {
-            throw new RuntimeException(LISTJVMS + " does not expect argument");
-        }
+    public void listJvms() {
         for (VmInfo vmInfo : vmManager.getVmInfoSet()) {
             System.out.println(vmInfo.getVmPid() + " " + vmInfo.getVmName());
         }
     }
-
-    private void printHelp() {
-        System.out.println("Allowed are: " + LISTJVMS + " , " + LISTPLUGINS + " , " + LISTCLASSES + ", " + BASE64 + " , " + BYTES + ", " + DECOMPILE + ", " + VERBOSE);
-        System.out.println(VERBOSE + " will set this app to print out all Exceptions and some debugging strings. Then continues ");
-        System.out.println(HELP + "/" + H + " print this help end exit");
-        System.out.println(LISTJVMS + " no arg expected, list available localhost JVMs ");
-        System.out.println(LISTPLUGINS + "  no arg expected, currently configured plugins with theirs status");
-        System.out.println(LISTCLASSES + "  one arg - pid or url of JVM. List its loaded classes.");
-        System.out.println(BYTES + "  two args - pid or url of JVM and class to obtain - will stdout its binary form");
-        System.out.println(BASE64 + "  two args - pid or url of JVM and class to obtain - will stdout its binary encoded as base64");
-        System.out.println(DECOMPILE + "  three args - pid or url of JVM and class to obtain and name/file of decompiler config - will stdout decompiled class");
-        System.out.println("              you can use special keyword javap, instead of decompiler name, to use javap disassembler.");
-        System.out.println("              You can pass also parameters to it like any other javap, but without space. So e.g. javap-v is equal to call javap -v /tmp/class_you_entered.class");
-        System.out.println("Allowed are: " + LISTJVMS + " , " + LISTPLUGINS + " , " + LISTCLASSES + ", " + BASE64 + " , " + BYTES + ", " + DECOMPILE + ", " + VERBOSE);
-    }
-
 
     private static String invalidityToString(boolean invalidWrapper) {
         if (invalidWrapper) {
--- runtime-decompiler/src/main/java/org/jrd/backend/data/Main.java
+++ runtime-decompiler/src/main/java/org/jrd/backend/data/Main.java
@@ -1,26 +1,99 @@
 package org.jrd.backend.data;
 
+import org.apache.commons.cli.*;
 import org.jrd.backend.core.OutputController;
 import org.jrd.frontend.MainFrame.MainFrameView;
 import org.jrd.frontend.MainFrame.VmDecompilerInformationController;
 
+import java.util.Arrays;
+
 public class Main {
 
 
-    public static void main(String[] allargs) throws Exception {
-        Model model = new Model();
-        Cli cli = new Cli(allargs, model);
-        if (cli.shouldBeVerbose()){
+    public static void main(String[] args){
+
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+
+        // Boolean
+        Option verboseOption = new Option("v", "verbose", false, "Be verbose");
+        verboseOption.setOptionalArg(true);
+        options.addOption(verboseOption);
+
+        // Main
+        OptionGroup mainOptions = new OptionGroup();
+        Option helpOption = new Option("h", "help", false, "Print this message");
+        mainOptions.addOption(helpOption);
+        Option versionOption = Option.builder()
+                .longOpt("version")
+                .desc("Print version information")
+                .build();
+        mainOptions.addOption(versionOption);
+        Option guiOption = new Option("g", "gui", false, "Launch GUI");
+        mainOptions.addOption(guiOption);
+        Option listjvmsOption = new Option("l", "listjvms", false, "List JVMs");
+        mainOptions.addOption(listjvmsOption);
+        Option listpluginsOption = new Option("p", "listplugins", false, "List plugins");
+        mainOptions.addOption(listpluginsOption);
+        Option listclassesOption = Option.builder("c")
+                .longOpt("listclasses")
+                .hasArg()
+                .argName("PID")
+                .desc("List classes")
+                .build();
+        mainOptions.addOption(listclassesOption);
+        Option decompileOption = Option.builder("d") // <PID> <CLASSNAME> <BASE64|BYTES|DecompilerName> [ARGS for decompiler]
+                .longOpt("decompile")
+                .numberOfArgs(3)
+                .argName("PID> <Classname> <decompiler> [args]") //this is dumb
+                .desc("Decompile class")
+                .build();
+        mainOptions.addOption(decompileOption);
+        options.addOptionGroup(mainOptions);
+
+        //Parse
+        CommandLine commandline;
+        try {
+            commandline = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("There was an error when parsing arguments: " + e.getMessage());
+            System.exit(1);
+            return;
+        }
+        // Verbose
+        if (commandline.hasOption("verbose")){
             OutputController.getLogger().setVerbose();
         }
-        if (cli.isGui()) {
+        //mainOptions
+        if (commandline.hasOption("help")){
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.setWidth(120);
+            formatter.printHelp("java-runtime-decompiler","",  options,
+                    "Please report issues at https://github.com/pmikova/java-runtime-decompiler/issues", true);
+        }
+        Model model = new Model();
+        if (commandline.hasOption("gui")){
             setLookAndFeel();
             MainFrameView mainView = new MainFrameView();
             VmDecompilerInformationController controller = new VmDecompilerInformationController(mainView, model);
-        } else {
-            cli.consumeCli();
         }
-
+        Cli Cli = new Cli(model);
+        if (commandline.hasOption("listjvms")){
+            Cli.listJvms();
+        }
+        if (commandline.hasOption("listplugins")){
+            Cli.listPlugins();
+        }
+        if (commandline.hasOption("listclasses")){
+            System.out.println("Listing classes for JVM with PID " + commandline.getOptionValue("listclasses") + ".");
+            System.out.println(Arrays.toString(commandline.getArgs()));
+//            Cli.listClasses();
+        }
+        if (commandline.hasOption("decompile")){
+            System.out.println(Arrays.toString(commandline.getOptionValues("decompile")));
+//            Cli.decompile();
+        }
+        System.exit(0);
     }
 
     public static void setLookAndFeel(){
